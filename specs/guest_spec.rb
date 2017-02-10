00a2589929d68b50577d6d264224baea9ee2d14b
Colin Farquhar <cifarquhar@gmail.com>
2017-02-10 13:16:44 +0000

    add money parameter to guest

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -5,12 +5,16 @@ require_relative('../guest.rb')
 class GuestSpec < MiniTest::Test
 
   def setup()
-    @guest1 = Guest.new("Colin")
+    @guest1 = Guest.new("Colin",100)
   end
 
 
   def test_guest_has_name
     assert_equal("Colin",@guest1.name)
+  end
+
+  def test_guest_has_money
+    assert_equal(100,@guest1.money)
   end
 
 
