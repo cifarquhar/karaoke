a90909bf952a4dcde3d2b4497a88a8710514275f
Colin Farquhar <cifarquhar@gmail.com>
2017-02-11 11:37:02 +0000

    testing drink prices in bar class

--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -13,6 +13,13 @@ class BarSpec < MiniTest::Test
     assert_equal(false,@bar1.drinks.empty?)
   end
 
+  def test_drink_prices
+    assert_equal(5,@bar1.drinks[:beer])
+    assert_equal(5,@bar1.drinks[:wine])
+    assert_equal(2,@bar1.drinks[:cider])
+    assert_equal(10,@bar1.drinks[:vodka])
+  end
+
 
 
 
